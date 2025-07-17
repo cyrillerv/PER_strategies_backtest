cbcd27f969096138945eae65944377cd9a634255
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-17 11:37:35 +0100

    We add stockPrices data

--- src/data_loading.py
+++ src/data_loading.py
@@ -7,9 +7,10 @@ def load_data() :
     df_PER = pd.read_excel(r"data\PER.xlsx")
     df_TotalAssets = pd.read_excel(r"data\TotalAssets.xlsx")
     df_TotalRevenue = pd.read_excel(r"data\TotalRevenue.xlsx")
+    df_stockPrices = pd.read_excel(r"data\StockPrices.xlsx")
 
 
     with open(r"data\CompanySectors.json", "r") as f:
         dic_sectors = json.load(f)
 
-    return df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, dic_sectors
+    return df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, df_stockPrices, dic_sectors
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def clean_data(df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, dic_sectors) :
+def clean_data(df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, df_stockPrices, dic_sectors) :
     df_MarketCap.set_index("Unnamed: 0", inplace=True)
     df_MarketCap.index.name = None
     df_MarketCap.replace([np.inf, -np.inf], np.nan, inplace=True)
@@ -13,6 +13,9 @@ def clean_data(df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, dic_sector
     df_PER.replace([np.inf, -np.inf], np.nan, inplace=True)
     df_PER.dropna(axis=1, how='all', inplace=True)
     df_PER.index = pd.to_datetime(df_PER.index)
+    # On reindex
+    df_PER_reindex = df_PER.reindex(pd.date_range(df_PER.index.min(), df_PER.index.max()), method='ffill')
+    # Enlever les PER négatifs
 
     df_TotalAssets.set_index("Unnamed: 0", inplace=True)
     df_TotalAssets.index.name = None
@@ -26,7 +29,13 @@ def clean_data(df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, dic_sector
     df_TotalRevenue.dropna(axis=1, how='all', inplace=True)
     df_TotalRevenue.index = pd.to_datetime(df_TotalRevenue.index)
 
-    return df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, dic_sectors
+    df_stockPrices.set_index("Unnamed: 0", inplace=True)
+    df_stockPrices.index.name = None
+    df_stockPrices.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df_stockPrices.dropna(axis=1, how='all', inplace=True)
+    df_stockPrices.index = pd.to_datetime(df_stockPrices.index)
+
+    return df_MarketCap, df_PER_reindex, df_TotalAssets, df_TotalRevenue, df_stockPrices, dic_sectors
 
 # We keep only the tickers for which we have data for everything => seulement pour clustering
 # tickers = set.intersection(set(df_MarketCap.columns), set(df_PER.columns), set(df_TotalAssets.columns), set(df_TotalRevenue.columns), set(dic_sectors.keys()))
