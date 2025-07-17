e865ed2579d3d0b83ea2cd6ac447315ecfe69373
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-17 15:58:41 +0100

    Add the classic clustering strategy

--- scripts/run_all_backtests.py
+++ scripts/run_all_backtests.py
@@ -10,6 +10,7 @@ from src.strategies.strategy_baseline import strat_fixed_treshold
 from src.strategies.strategy_historical import historical_PER_strat
 from src.strategies.strategy_sector import strat_sector_PER
 from src.strategies.strategy_distance_matrix_clustering import strategy_distance_matrix_clustering
+from src.strategies.strategy_cluster import strat_cluster_K_means
 
 df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, df_stockPrices, dic_sectors = load_data()
 df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, df_stockPrices, dic_sectors = clean_data(df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, df_stockPrices, dic_sectors)
@@ -20,4 +21,5 @@ num_stocks_available = 1000 // df_stockPrices
 # strat_fixed_treshold(df_PER, num_stocks_available)
 # historical_PER_strat(df_PER, num_stocks_available)
 # strat_sector_PER(df_PER, dic_sectors, num_stocks_available)
-print(strategy_distance_matrix_clustering(df_MarketCap, df_TotalAssets, df_TotalRevenue, df_PER, dic_sectors, num_stocks_available))
+# strategy_distance_matrix_clustering(df_MarketCap, df_TotalAssets, df_TotalRevenue, df_PER, dic_sectors, num_stocks_available)
+strat_cluster_K_means(df_TotalRevenue, df_TotalAssets, df_MarketCap, df_PER, dic_sectors, num_stocks_available)
---
+++ src/strategies/strategy_cluster.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from src.utils.utils_clustering import prepare_field_data, calculate_cluster, interpret_signals
+
+def strat_cluster_K_means(df_TotalRevenue, df_TotalAssets, df_MarketCap, df_PER, dic_sectors, num_stocks_available) :
+    # TODO: passer ces lignes ci dessous dans le fichier preprocessing.py
+    df_TotalRevenue.ffill(inplace=True)
+    df_TotalAssets.ffill(inplace=True)
+    df_MarketCap.ffill(inplace=True)
+    df_PER.ffill(inplace=True)
+    # On fusionne toutes les infos en un seul df
+    melted_MarketCap = prepare_field_data(df_MarketCap, 'MarketCap')
+    melted_TotalRevenue = prepare_field_data(df_TotalRevenue, 'TotalRevenue')
+    melted_TotalAssets = prepare_field_data(df_TotalAssets, 'TotalAssets')
+    melted_PER = prepare_field_data(df_PER, 'PER')
+
+    df_input = pd.concat([melted_MarketCap, melted_TotalRevenue, melted_TotalAssets, melted_PER], axis=1).dropna()
+    df_input.reset_index(inplace=True)
+    print("df_input created")
+
+    label_sector = {i:k for k, i in enumerate(list(set(dic_sectors.values())))}
+    df_input["Sector"] = df_input['Ticker'].map(dic_sectors).map(label_sector)
+    df_input.dropna(inplace=True)
+
+    print("Cluster calc")
+    df_clustered = calculate_cluster(df_input)
+    print(df_clustered)
+
+    print("Interpreting signals")
+    df_prise_position_clustering = interpret_signals(df_clustered, 0.2, num_stocks_available)
+    return df_prise_position_clustering
