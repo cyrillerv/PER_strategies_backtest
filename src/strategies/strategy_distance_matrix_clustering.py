748f572b44dc29d797d5b7f2411c2def873d94a4
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-17 15:58:59 +0100

    minor changes

--- src/strategies/strategy_distance_matrix_clustering.py
+++ src/strategies/strategy_distance_matrix_clustering.py
@@ -4,9 +4,10 @@ from tqdm import tqdm
 tqdm.pandas()
 from functools import partial
 
-from src.utils.distance_matrix_utils import *
+from src.utils.utils_clustering import *
 
-
+# TODO: enlever le système d'excel car df trop grand
+# TODO: ne pas recalculer les cluster pour chaque date, mais plutôt le faire pour un intervalle plus long. Ex: trimestriellement.
 
 
 def strategy_distance_matrix_clustering(df_MarketCap, df_TotalAssets, df_TotalRevenue, df_PER, dic_sectors, num_stocks_available) :
@@ -39,7 +40,10 @@ def strategy_distance_matrix_clustering(df_MarketCap, df_TotalAssets, df_TotalRe
     df_all_cluster_distance_matrix["PER"] = df_all_cluster_distance_matrix.progress_apply(lambda row: reindexed_PER.loc[row["date"], row["Ticker"]] if row["date"] in reindexed_PER.index and row["Ticker"] in reindexed_PER.columns else np.nan, axis=1)
     df_all_cluster_distance_matrix.dropna(inplace=True)
 
+    nb_min_ticker_per_cluster = 5
+    index_ok_cluster = df_all_cluster_distance_matrix.groupby(["date", "Cluster"]).count()['Ticker'][df_all_cluster_distance_matrix.groupby(["date", "Cluster"]).count()['Ticker'] > nb_min_ticker_per_cluster].index
+    df_all_cluster_distance_matrix = df_all_cluster_distance_matrix.set_index(["date", "Cluster"])[df_all_cluster_distance_matrix.set_index(["date", "Cluster"]).index.isin(index_ok_cluster)].reset_index()
     # Ensuite pour chaque entreprise on va acheter ou vendre en fonction du PER des autres entreprises dans le cluster
-    df_prise_positions_distance_matrix = interpret_signals(df_all_cluster_distance_matrix, 5, 0.2, num_stocks_available)
+    df_prise_positions_distance_matrix = interpret_signals(df_all_cluster_distance_matrix, 0.2, num_stocks_available)
     return df_prise_positions_distance_matrix
 
