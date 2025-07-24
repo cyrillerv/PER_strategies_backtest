eb1db5010ce2802ca6d55f7d45919f02165c21a4
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-24 11:20:40 +0100

    config file to input our parameters

--- scripts/run_all_backtests.py
+++ scripts/run_all_backtests.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pandas as pd 
+import json
 from backtesting.core import BacktestEngine # Librairy I created available on GitHub
 
 
@@ -15,13 +16,19 @@ from src.strategies.strategy_sector import strat_sector_PER
 from src.strategies.strategy_distance_matrix_clustering import strategy_distance_matrix_clustering
 from src.strategies.strategy_cluster import strat_cluster_K_means
 
-fields = ["MarketCap", "PER", "TotalAssets", "TotalRevenue", "StockPrices", "Sectors"]
+
+# We load our parameters from our config.json file
+with open('config.json', 'r') as f:
+    config = json.load(f)
+rebalancing_dates = config['rebalancing_dates']
+fields = config["fields"]
+money_per_transaction = config["money_per_transaction"]
+
 print("Loading and prepararing data...")
 dic_results = load_data(fields)
 dic_results = clean_data(dic_results)
-# df_MarketCap, df_PER, df_TotalAssets, df_TotalRevenue, df_stockPrices, dic_sectors = tuple(dic_results[field] for field in fields)
-# Nombre d'actions qu'on peut s'acheter à chaque date avec 1000$
-num_stocks_available = 1000 // dic_results['StockPrices']
+# Nombre d'actions qu'on peut s'acheter à chaque date avec x$
+num_stocks_available = money_per_transaction // dic_results['StockPrices']
 
 print("Launching fixedThreshold...")
 order_table_fixed_threshold_strat = strat_fixed_treshold(dic_results, num_stocks_available)
@@ -29,18 +36,19 @@ print("Launching histoStrat...")
 order_table_historical_strat = historical_PER_strat(dic_results, num_stocks_available)
 print("Launching sectorStrat...")
 order_table_sector_strat = strat_sector_PER(dic_results, num_stocks_available)
-print("Launching distanceMatrix...")
-order_table_distance_matrix_strat = strategy_distance_matrix_clustering(dic_results, num_stocks_available)
 print("Launching clustering...")
 order_table_clustering_strat = strat_cluster_K_means(dic_results, num_stocks_available)
+print("Launching distanceMatrix...")
+order_table_distance_matrix_strat = strategy_distance_matrix_clustering(dic_results, num_stocks_available, rebalancing_dates)
+
 
 
 dic_launch_bt = {
     "fixedThreshold": order_table_fixed_threshold_strat,
     "histoStrat": order_table_historical_strat,
     "sectorStrat": order_table_sector_strat,
-    "distanceMatrix": order_table_distance_matrix_strat,
     "clustering": order_table_clustering_strat,
+    "distanceMatrix": order_table_distance_matrix_strat,
 }
 
 dic_results_bt = {}
--- scripts/run_one_strategy.py
+++ scripts/run_one_strategy.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import json
 # Ajoute le dossier racine du projet au PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -12,21 +12,36 @@ from src.strategies.strategy_sector import strat_sector_PER
 from src.strategies.strategy_distance_matrix_clustering import strategy_distance_matrix_clustering
 from src.strategies.strategy_cluster import strat_cluster_K_means
 
+# We load our parameters from our config.json file
+with open('config.json', 'r') as f:
+    config = json.load(f)
+rebalancing_dates = config["rebalancing_dates"]
+money_per_transaction = config["money_per_transaction"]
+
 strat = strategy_distance_matrix_clustering
 
 dic_strat_data = {
     strat_fixed_treshold : ["PER", "StockPrices"],
     historical_PER_strat : ["PER", "StockPrices"],
     strat_sector_PER : ["PER", "StockPrices"],
-    strategy_distance_matrix_clustering : ['MarketCap', "TotalAssets", "TotalRevenue", "PER", "Sectors", "StockPrices"],
-    strat_cluster_K_means : ['MarketCap', "TotalAssets", "TotalRevenue", "PER", "Sectors", "StockPrices"]
+    strat_cluster_K_means : ['MarketCap', "TotalAssets", "TotalRevenue", "PER", "Sectors", "StockPrices"],
+    strategy_distance_matrix_clustering : ['MarketCap', "TotalAssets", "TotalRevenue", "PER", "Sectors", "StockPrices"]    
 }
 
 fields = list(dic_strat_data[strat])
 dic_results = load_data(fields)
 dic_results = clean_data(dic_results)
-num_stocks_available = 1000 // dic_results['StockPrices']
+num_stocks_available = money_per_transaction // dic_results['StockPrices']
 
-result = strat(dic_results, num_stocks_available)
+dic_args = {
+    strat_fixed_treshold : (dic_results, num_stocks_available),
+    historical_PER_strat : (dic_results, num_stocks_available),
+    strat_sector_PER : (dic_results, num_stocks_available),
+    strat_cluster_K_means : (dic_results, num_stocks_available),
+    strategy_distance_matrix_clustering : (dic_results, num_stocks_available)
+}
+
+args = dic_args[strat]
+result = strat(*args)
 
 
